16b108b8be391f9e00d851a331f326490ce75ef5
NahomBirhane <nahomyatus@gmail.com>
2025-04-10 18:21:04 -0500

    initial Cut

--- Main.java
+++ Main.java
@@ -1,8 +1,34 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        QuestionService service = new QuestionService();
-        service.displayQuestions();
+        Scanner scanner = new Scanner(System.in);
+        QuestionService questionService = new QuestionService();
+        Question[] questions = questionService.getQuestions();
+
+        int score = 0;
+
+        System.out.println("Welcome to the Java Quiz!\n");
+
+        for (Question q : questions) {
+            System.out.println(q.getQuestion());
+            System.out.println(q.getOpt1());
+            System.out.println(q.getOpt2());
+            System.out.println(q.getOpt3());
+            System.out.println(q.getOpt4());
+
+            System.out.print("Your answer (A/B/C/D): ");
+            String userAnswer = scanner.nextLine().toUpperCase();
+
+            if (userAnswer.equals(q.getAnswer())) {
+                System.out.println("Correct!\n");
+                score++;
+            } else {
+                System.out.println("Wrong! Correct answer: " + q.getAnswer() + "\n");
+            }
+        }
+
+        System.out.println("Quiz finished!");
+        System.out.println("Your score: " + score + " out of " + questions.length);
     }
 }
--- QuestionService.java
+++ QuestionService.java
@@ -1,14 +1,15 @@
 public class QuestionService {
+    private Question[] questions = new Question[5];
 
-    Question[] questions = new Question[5];
-
-    public QuestionService(){
-        questions[0]=new Question(1,"what is java","A","B","c","D","D");questions[1]=new Question(1,"what is REACT","A","B","c","D","D");questions[2]=new Question(1,"what is OOP","A","B","c","D","A");questions[3]=new Question(1,"what is SPRING BOOT","A","B","c","D","B");questions[4]=new Question(1,"what is life","A","B","c","D","A");
+    public QuestionService() {
+        questions[0] = new Question(1, "What is Java?", "A. Programming Language", "B. Snake", "C. Bird", "D. Coffee", "A");
+        questions[1] = new Question(2, "What is React?", "A. Backend Tool", "B. JavaScript Library", "C. Database", "D. OS", "B");
+        questions[2] = new Question(3, "What does OOP stand for?", "A. Out Of Print", "B. Object Oriented Programming", "C. Only One Program", "D. Open Office Project", "B");
+        questions[3] = new Question(4, "Which framework is used for Java backend?", "A. Django", "B. Spring Boot", "C. Laravel", "D. Flask", "B");
+        questions[4] = new Question(5, "Which one is a relational database?", "A. MongoDB", "B. Firebase", "C. PostgreSQL", "D. Redis", "C");
     }
 
-    public void displayQuestions() {
-        for (Question q : questions) {
-            System.out.println(q);
-        }
+    public Question[] getQuestions() {
+        return questions;
     }
 }
